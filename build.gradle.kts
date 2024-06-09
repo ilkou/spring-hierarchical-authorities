617f5e97185492728852adbb4f3f6a1f837bdc76
Ouklich Oussama <48165230+ilkou@users.noreply.github.com>
2024-06-10 00:59:14 +0100

    fix vul issue regarding ipaddress

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,12 +21,22 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 
 	// https://mvnrepository.com/artifact/org.casbin/casbin-spring-boot-starter
-	implementation("org.casbin:casbin-spring-boot-starter:1.4.0")
+	implementation("org.casbin:casbin-spring-boot-starter:1.5.0")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+configurations.all {
+	resolutionStrategy {
+		eachDependency {
+			if (requested.group == "org.casbin" && requested.name == "jcasbin") {
+				useVersion("1.55.0") // Specify the version you want to use
+			}
+		}
+	}
 }
 
 tasks.withType<Test> {
